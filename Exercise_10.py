c24e71740ba98983e1f7ce2c7b944dceeb352a87
Gokula Krishnan R <109400792+gkrcodes@users.noreply.github.com>
2024-01-04 19:09:54 +0800

    Add files via upload

---
+++ Exercise_10.py
@@ -0,0 +1,15 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "Iam split\non a line"
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* cat food
+\t* Fishes
+\t* catnip\n\t* Grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
